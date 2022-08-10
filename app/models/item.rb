c6ebb331a2aa5b97a35e4d96fde74942a7522616
hso-sue <hso112277@yahoo.co.jp>
2022-08-10 15:31:16 +0900

    ActiveHashを導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :postage_payer
+  belongs_to :sender_area
+  belongs_to :preparation
+
+  validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_payer_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :sender_area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :preparation_id, numericality: { other_than: 1, message: "can't be blank" }
 end
