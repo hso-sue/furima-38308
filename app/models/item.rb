9b9fc188a00de966ecc4a993822654944b780f95
hso-sue <hso112277@yahoo.co.jp>
2022-08-10 16:02:25 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,9 +9,14 @@ class Item < ApplicationRecord
   belongs_to :sender_area
   belongs_to :preparation
 
-  validates :item_category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image,     presence: true
+  validates :item_name, presence: true
+  validates :introduction, presence: true
+  validates :item_category_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :postage_payer_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :sender_area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :preparation_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postage_payer_id,  numericality: { other_than: 1, message: "can't be blank" }
+  validates :sender_area_id,    numericality: { other_than: 1, message: "can't be blank" }
+  validates :preparation_id,    numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
+
 end
