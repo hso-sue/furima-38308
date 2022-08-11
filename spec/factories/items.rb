53c922c28dd257ff86c798d36e45b6c7c9dcb5df
hso-sue <hso112277@yahoo.co.jp>
2022-08-11 13:48:19 +0900

    販売手数料・利益計算のJavaScript実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { Faker::Name.name }
+    introduction { Faker::Lorem.sentence }
+    item_category_id {'6'}
+    item_condition_id {'3'}
+    postage_payer_id {'2'}
+    sender_area_id {'40'}
+    preparation_id {'3'}
+    price {'66666'}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test.png'), filename: 'test.png')
+    end
   end
 end
