c0fc2ad45c9d18dc9136bf251c584f152dfb4172
hso-sue <hso112277@yahoo.co.jp>
2022-08-11 15:29:03 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,14 @@ class Item < ApplicationRecord
   belongs_to :sender_area
   belongs_to :preparation
 
-  validates :image,     presence: true
-  validates :item_name, presence: true
-  validates :introduction, presence: true
+  validates :image,             presence: true
+  validates :item_name,         presence: true
+  validates :introduction,      presence: true
   validates :item_category_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :item_condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :postage_payer_id,  numericality: { other_than: 1, message: "can't be blank" }
   validates :sender_area_id,    numericality: { other_than: 1, message: "can't be blank" }
   validates :preparation_id,    numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, allow_blank: true
-  validates :price, format: { with: /\A[0-9]+\z/ }
+  validates :price,             numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, allow_blank: true
+  validates :price,             format: { with: /\A[0-9]+\z/ }
 end
